00bf0447b7927441f171ef77d44f228bd00833b1
Himank Jain <himankjain@yahoo.in>
2022-06-16 22:57:03 +0530

    type casting example

--- types/src/main.rs
+++ types/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
+
+   let decimal_number = 45.643;
+   //type conversion
+   let int_number = decimal_number as u8;
+
+   println!("Decimal Number {}", decimal_number);
+   println!("Casted Number {}", int_number);
+
+   //inference
    let big_number:u64 = 12343534563445656646;
 
    type Color = u32;
