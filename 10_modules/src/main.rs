5139de6364aaf32b97d7b012c76c1e6eb34b9e5a
Himank Jain <himankjain@yahoo.in>
2022-07-02 14:30:15 +0530

    Crate and module

--- 10_modules/src/main.rs
+++ 10_modules/src/main.rs
@@ -1,9 +1,15 @@
 mod bird;
-
-
+//Imported local create from '11_crates' folder
+//use could limit to crate, module or down to function 
+use crates::lib_nat::intro;
 
 fn main() {
 
 bird::intro();
 
+//Called via long path form
+//crates::lib_nat::intro();
+
+//Called via short form
+intro();
 }
---
+++ 11_crates/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod lib_nat {
+    pub fn intro() {
+    println!("I am invoked from a lib");
+}
+}
